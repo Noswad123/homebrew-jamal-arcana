f075488eab43529089b063c8b94a439dfca70925
Jamal Dawson <Jamal.Dawson.@modernatx.com>
2026-06-18 20:24:20 -0400

    Update Jamal Arcana releases

--- Formula/mw.rb
+++ Formula/mw.rb
@@ -1,8 +1,8 @@
 class Mw < Formula
   desc "Local-first notes, todos, and optional Hive Mind sync CLI"
   homepage "https://github.com/Noswad123/mind-weaver"
-  url "https://github.com/Noswad123/mind-weaver/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "26be083437a4d7eddab85db1cf2e7fcdf8245a8af5c8453474edc6db6c54646b"
+  url "https://github.com/Noswad123/mind-weaver/archive/refs/tags/v0.1.3.tar.gz"
+  sha256 "e2ef2f13a3d0af06f61ef0f86f347817dba818f70121eea1abacbb3599c4e0a6"
   license "MIT"
   head "https://github.com/Noswad123/mind-weaver.git", branch: "main"
 
